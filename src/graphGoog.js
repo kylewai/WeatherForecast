c0ea3fff234026799d5a9eebdf9ee85b65f6d8f9
kylewai <kwaifss@ufl.edu>
2018-08-27 23:06:24 -0400

    hello

---
+++ src/graphGoog.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import {GoogleCharts} from 'google-charts';
+
+var chart;
+class GraphGoog extends Component{
+
+  componentDidMount(){
+    GoogleCharts.load(() => {this.drawChart()});
+  }
+
+  componentDidUpdate(){
+    GoogleCharts.load(() => {this.drawChart()});
+  }
+
+  drawChart(){
+    var data = new GoogleCharts.api.visualization.DataTable();
+    data.addColumn('timeofday', 'Hour');
+    data.addColumn('number', 'Temperature');
+    var weat = [];
+    this.props.weatherData.map((i) => {
+        var hour = parseInt(i.time.substr(0, 2));
+        var minute = parseInt(i.time.substr(3, 5));
+        var seconds = 0;
+        weat.push([[hour, minute, seconds], parseInt(i.temp)])
+    });
+    data.addRows(weat);
+    chart = new GoogleCharts.api.visualization.LineChart(document.getElementById('chart'));
+    var options = {
+      width: 700,
+      height: '100%',
+      chartArea: {left: 35, 'width': '100%', 'height': '70%'},
+      backgroundColor: {fill: 'transparent'}
+    }
+    chart.draw(data, options);
+  }
+
+  render(){
+    return(
+      <div id = 'chart'></div>
+    );
+  }
+}
+export default GraphGoog;
